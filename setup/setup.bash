5764f12f81e219643fded6e20d7a345abe2b0b83
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-12-12 16:18:05 +0100

    Updated launch files; added amigo teleop

--- setup/setup.bash
+++ setup/setup.bash
@@ -1,2 +1,4 @@
 alias rviz-amigo='rosrun rviz rviz -d `rospack find amigo_ed_tutorials`/config/rviz/amigo_ed.rviz'
+alias amigo-teleop='rosrun amigo_ed_tutorials teleop.py /cmd_vel:=/amigo/base/references'
+
 export ED_PLUGIN_PATH=`rospack find ed`/../../devel/lib
